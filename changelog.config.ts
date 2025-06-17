1ef3f5f34aa3578f9a8c142a27a1e3214822e25a
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-06-17 23:36:28 +0900

    🔧 chore: add changelog configuration file and release script

---
+++ changelog.config.ts
@@ -0,0 +1,23 @@
+import type { ChangelogConfig } from 'changelogen';
+
+export default () =>
+  ({
+    types: {
+      feat: { title: '✨ Enhancements', semver: 'minor' },
+      perf: { title: '⚡️ Performance', semver: 'patch' },
+      update: { title: '🎨 Improvements', semver: 'patch' },
+      fix: { title: '🩹 Fixes', semver: 'patch' },
+      hotfix: { title: '🚑 Critical Bug Fix', semver: 'patch' },
+      refactor: { title: '♻️ Refactors', semver: 'patch' },
+      delete: { title: '🔥 Deletion', semver: 'patch' },
+      type: { title: '🏷️ Types', semver: 'patch' },
+      deps: { title: '📦️ Dependencies', semver: 'patch' },
+      docs: { title: '📝 Documentation' },
+      test: { title: '✅ Tests' },
+      chore: { title: '🔧 Chore' },
+      ci: { title: '🤖 CI' },
+    },
+    templates: {
+      commitMessage: ':bookmark: chore(release): v{{newVersion}}',
+    },
+  } satisfies Partial<ChangelogConfig>);
