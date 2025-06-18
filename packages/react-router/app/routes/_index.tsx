06badac1cacd863c9c6620fb072478fb3c31b5ba
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-06-18 20:50:05 +0900

    🔧 chore: update @biomejs/biome dependency to version 1.9.4 and adjust meta function parameter

--- packages/react-router/app/routes/_index.tsx
+++ packages/react-router/app/routes/_index.tsx
@@ -1,8 +1,7 @@
 import { Welcome } from '../welcome/welcome';
 import type { Route } from './+types/_index';
 
-// biome-ignore lint/correctness/noEmptyPattern: <explanation>
-export function meta({}: Route.MetaArgs) {
+export function meta(_: Route.MetaArgs) {
   return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
