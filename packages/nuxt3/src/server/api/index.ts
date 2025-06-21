7bb646192e18a062258cf3f77c6f35c3dd8d3d92
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-06-21 19:57:37 +0900

    :sparkles: feat(nuxt3): add main type exports for API package

---
+++ packages/nuxt3/src/server/api/index.ts
@@ -0,0 +1,13 @@
+/**
+ * API Package Index
+ *
+ * Main entry point for the API package.
+ * Re-exports commonly used types, utilities, and constants.
+ */
+
+export * from './types';
+export * from './constants/http-status';
+export * from './types/api-response';
+export * from './schemas/common';
+export * from './utils/system-info';
+export * from './utils/text-formatting';
