5f94fdeb11ab2b469da5d7e2052de7dcab990121
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-06-29 23:05:10 +0900

    🔧 chore(tanstack): add linter settings

---
+++ packages/tanstack/prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
