5fdbeeb9a6b264e38ee3929a9482d7c86ccd4c34
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-06-29 23:26:23 +0900

    🔧 feat(tanstack): add vitest configuration and testing scripts to package.json

---
+++ packages/tanstack/vitest.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  test: {
+    environment: 'happy-dom',
+    reporters: ['verbose'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'clover', 'json'],
+      reportsDirectory: './coverage',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': `${__dirname}/app`,
+    },
+  },
+});
